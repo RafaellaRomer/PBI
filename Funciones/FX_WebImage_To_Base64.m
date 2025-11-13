7a124753139d29b9a6d3f356f9267e2313ec4c15
Rafaella Romer <rafaella.rtg@gmail.com>
2025-11-13 10:20:42 +0100

    Create FX_WebImage_To_Base64.m

---
+++ Funciones/FX_WebImage_To_Base64.m
@@ -0,0 +1,18 @@
+// To use this code:
+  // invoque the fuction as a step after the table to agregate the base64 column fx_webImg_toBase64 (table_previuos_step)
+  // in web_contents = define the name of the column yhat contains the web link [Col_Contains_Image_Link]
+  // in web_contents and binary_toText define the "Column_Name1" and "Column_Name2"
+  // keep or remove the quita_col step, it removes the non more usable initial and created columns
+  
+
+fx_webImg_toBase64 = (tabela as table) as table =>
+       let
+           web_contents = Table.AddColumn(tabela, "Column_Name1", each Web.Contents([Col_Contains_Image_Link])),  
+           binary_toText = Table.AddColumn(web_contents, "Column_Name2", each "data:image/png;base64, " & Binary.ToText([Icon_Web], BinaryEncoding.Base64)),
+           quita_col = Table.RemoveColumns(binary_toText, {"Column_Name1", "Col_Contains_Image_Link"})
+       in
+           quita_col
+
+
+// put conditions to not return return errors
+
