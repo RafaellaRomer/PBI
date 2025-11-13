f3fa0fb7f498c865395de00e9874948d076a12fc
Rafaella Romer <rafaella.rtg@gmail.com>
2025-11-13 14:29:43 +0100

    Update Initial_Steps_NOT_Hard_Coding.m

--- M/Initial_Steps_NOT_Hard_Coding.m
+++ M/Initial_Steps_NOT_Hard_Coding.m
@@ -1,29 +1,45 @@
-//  CSV. DOCUMENT
+//  CSV. DOCUMENT() step ->binary to table
 
 let
-    Origen = SharePoint.Files(ruta_shp, [ApiVersion = 15]),
-    filtra_carpeta = Table.SelectRows(Origen, each 
-    [Folder Path] = ruta_shp & ruta_carpeta 
-    and [Attributes]?[Hidden]? <> true
-    and [Extension] = ".csv"),
-    csv = Table.TransformColumns(filtra_carpeta, 
-        {
-            {"Content", each Csv.Document(_,[Delimiter=";", Columns=12, Encoding=65001, QuoteStyle=QuoteStyle.None]) }
-        }
+
+  Origen = SharePoint.Files(ruta_shp, [ApiVersion = 15]),
+
+  filtra_carpeta = Table.SelectRows(
+    Origen,
+    each [Folder Path] = ruta_shp & ruta_carpeta and [Extension] = ".csv"
+  ),
+
+  csv = Table.TransformColumns(
+    filtra_carpeta,
+    {
+      {
+        "Content",
+        each Csv.Document(
+          _,
+          [Delimiter = ";", Columns = 12, Encoding = 65001, QuoteStyle = QuoteStyle.None]
         )
+      }
+   }
+),
+
+  Content = csv[[Content]], // lista de tablas de una carpeta, donde se aplica la funcion de tabla si hay
+
+  tbl = Content[Content]{0}, // para acceder conteuno primer archivo, primer paso de una funcion de tabla
+
 
 in
-    csv
+
+  tbl
+ 
 
 
-// EXCEL.WORKBOOK
+// EXCEL.WORKBOOK step ->binary to table
 
 let
     Origen = SharePoint.Files(ruta_shp, [ApiVersion = 15]),
     filtra_carpeta = Table.SelectRows(Origen, each 
     [Folder Path] = ruta_shp & ruta_carpeta 
-    and [Attributes]?[Hidden]? <> true
-    and [Extension] = ".csv"),
+     and [Extension] = ".xslx"),
     csv = Table.TransformColumns(filtra_carpeta, 
         {
             {"Content", each Excel.Workbok(_) }
